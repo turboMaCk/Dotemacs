577d8244352c98d1869fbcb6fb46fd9beff7401c
Marek Fajkus <marek.faj@gmail.com>
2015-12-27 21:13:06 +0100

    evil to use-package

--- config/init-evil.el
+++ config/init-evil.el
@@ -1,22 +1,32 @@
-;; ~/emacs.d/config/init-evil.el
-(require 'evil)
-(evil-mode 1)
-(setq evil-search-module 'evil-search)
-(setq evil-magic 'very-magic)
+;; TODO: ex define Wq && WQ
+(use-package evil
+  :ensure t
+  :init
+  (progn
+    (evil-mode 1)
+    (setq evil-search-module 'evil-search)
+    (setq evil-magic 'very-magic))
+  :config
+  (progn
 
-;; Custom commpad
-(evil-ex-define-cmd "W[rite]" 'evil-write)
-(evil-ex-define-cmd "Q[uit]" 'evil-quit)
+    "Custom commands"
+    (evil-ex-define-cmd "W[rite]" 'evil-write)
+    (evil-ex-define-cmd "Q[uit]" 'evil-quit)
+    (evil-ex-define-cmd "Wq[rite]" 'evil-write)))
 
-(require 'evil-commentary)
-(evil-commentary-mode t)
 
-(require 'evil-leader)
-(global-evil-leader-mode)
+(use-package evil-commentary
+  :ensure t
+  :init (evil-commentary-mode t))
+
+(use-package evil-leader
+  :ensure t
+  :init (global-evil-leader-mode))
 
 ;; optional: this is the evil state that evil-magit will use
 ;; (setq evil-magit-state 'motion)
 ;; optional: enable additional bindings for yanking text (evil-magit-state needs
 ;; to be 'normal for this to work properly)
 ;; (setq evil-use-y-for-yank t)
-(require 'evil-magit)
+(use-package evil-magit
+  :ensure t)
