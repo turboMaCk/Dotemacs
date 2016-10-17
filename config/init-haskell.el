85d0c1e9d431ebc019b415f923cec8b6d845398f
Marek Fajkus <marek.faj@gmail.com>
2016-10-17 23:05:43 +0200

    setup magit

--- config/init-haskell.el
+++ config/init-haskell.el
@@ -1,3 +1,2 @@
 (use-package haskell-mode
   :ensure t)
-(require 'haskell-mode-autoloads)
--- init.el
+++ init.el
@@ -23,15 +23,15 @@
     (load "~/.emacs.d/config/init-term.el")
     (load "~/.emacs.d/config/init-modeline.el")
     (load "~/.emacs.d/config/init-interface.el")
+    (load "~/.emacs.d/config/init-linting.el")
 
     "Essentials"
+    (load "~/.emacs.d/config/init-auto-complete.el")
     (load "~/.emacs.d/config/init-evil.el")
     (load "~/.emacs.d/config/init-helm.el")
-    (load "~/.emacs.d/config/init-auto-complete.el")
     (load "~/.emacs.d/config/init-magit.el")
     (load "~/.emacs.d/config/init-org.el")
     (load "~/.emacs.d/config/init-yasnippet.el")
-    (load "~/.emacs.d/config/init-linting.el")
 
     "Languages support"
     (load "~/.emacs.d/config/init-web.el")
@@ -47,19 +47,3 @@
 
     "Glue"
     (load "~/.emacs.d/config/init-bindings.el")))
-
-;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("94ba29363bfb7e06105f68d72b268f85981f7fba2ddef89331660033101eb5e5" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
