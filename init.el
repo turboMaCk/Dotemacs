f5af71a7fd1bd7b90d8bf51d9ab9a44acbbf83c3
Marek Fajkus <marek.faj@gmail.com>
2017-07-23 21:00:43 +0200

    smaller improvements

--- init.el
+++ init.el
@@ -52,7 +52,7 @@
  '(haskell-tags-on-save t)
  '(package-selected-packages
    (quote
-    (caml shm idris-mode bash-completion psc-ide psci purescript-mode helm-css-scss perspective which-key evil-org org-evil hindent robe multiple-cursors ob-restclient restclient nyan-mode)))
+    (cargo caml shm idris-mode bash-completion psc-ide psci purescript-mode helm-css-scss perspective which-key evil-org org-evil hindent robe multiple-cursors ob-restclient restclient nyan-mode)))
  '(purescript-mode-hook (quote (turn-on-purescript-indent)) t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
